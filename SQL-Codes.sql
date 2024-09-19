5a0ec6dd4efcf94f0815768a2da53dcb16341416
Arthur Manske <130318764+arthur-manske@users.noreply.github.com>
2024-09-19 20:58:36 +0000

    Update SQL-Codes.sql

--- SQL-Codes.sql
+++ SQL-Codes.sql
@@ -1,70 +1,69 @@
--- Tabela de Usuários
 CREATE TABLE users (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(510) NOT NULL,
-   email VARCHAR(254) NOT NULL UNIQUE,
-   password VARCHAR(256) NOT NULL,
-   createdAt DATETIME NOT NULL,
-   description TEXT,
-   gender VARCHAR(62),
-   premium DATETIME,
-   userType INT NOT NULL
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(510) NOT NULL,
+    email VARCHAR(254) NOT NULL UNIQUE,
+    password VARCHAR(256) NOT NULL,
+    description TEXT,
+    gender VARCHAR(62),
+    premium DATETIME,
+    userType INT NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    editedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
- 
+
 -- Tabela de Aulas
 CREATE TABLE classes (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   instructorId INT NOT NULL,
-   title VARCHAR(255) NOT NULL,
-   description TEXT,
-   createdAt DATETIME NOT NULL,
-   willHappenAt DATETIME NOT NULL,
-   cost DECIMAL(10, 2) NOT NULL,
-   extraFiles TEXT[],
-   tags TEXT[],
-   FOREIGN KEY (instructorId) REFERENCES users(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    instructorId INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    happensAt DATETIME NOT NULL,
+    cost DECIMAL(10, 2) NOT NULL,
+    extraFiles TEXT,
+    tags TEXT,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    editedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (instructorId) REFERENCES users(id)
 );
- 
--- Tabela de Compras
+
 CREATE TABLE sells (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   studentId INT NOT NULL,
-   classId INT NOT NULL,
-   soldAt DATETIME NOT NULL,
-   cost DECIMAL(10, 2) NOT NULL,
-   FOREIGN KEY (studentId) REFERENCES users(id),
-   FOREIGN KEY (classId) REFERENCES classes(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    studentId INT NOT NULL,
+    classId INT NOT NULL,
+    cost DECIMAL(10, 2) NOT NULL,
+    soldAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (studentId) REFERENCES users(id),
+    FOREIGN KEY (classId) REFERENCES classes(id)
 );
- 
--- Tabela de Avaliações
+
 CREATE TABLE evaluations (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   classId INT NOT NULL,
-   studentId INT NOT NULL,
-   note DECIMAL(1, 1) NOT NULL,
-   contents TEXT,
-   editedAt DATETIME NOT NULL,
-   FOREIGN KEY (classId) REFERENCES classes(id),
-   FOREIGN KEY (studentId) REFERENCES users(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    classId INT NOT NULL,
+    studentId INT NOT NULL,
+    note DECIMAL(2, 1) NOT NULL CHECK (note >= 0 AND note <= 10),
+    contents TEXT,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    editedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (classId) REFERENCES classes(id),
+    FOREIGN KEY (studentId) REFERENCES users(id)
 );
- 
--- Tabela de Mensagens
+
 CREATE TABLE messages (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   senderId INT NOT NULL,
-   recipientId INT NOT NULL,
-   contents TEXT NOT NULL,
-   sendedAt DATETIME NOT NULL,
-   FOREIGN KEY (senderId) REFERENCES users(id),
-   FOREIGN KEY (recipientId) REFERENCES users(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    senderId INT NOT NULL,
+    recipientId INT NOT NULL,
+    contents TEXT NOT NULL,
+    sentAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (senderId) REFERENCES users(id),
+    FOREIGN KEY (recipientId) REFERENCES users(id)
 );
- 
--- Tabela de Fórum
-CREATE TABLE forum (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   authorId INT NOT NULL,
-   title VARCHAR(255) NOT NULL,
-   contents TEXT NOT NULL,
-   postedAt DATETIME NOT NULL,
-   FOREIGN KEY (authorId) REFERENCES users(id)
+
+CREATE TABLE forum_posts (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    authorId INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    contents TEXT NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    editedAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (authorId) REFERENCES users(id)
 );
