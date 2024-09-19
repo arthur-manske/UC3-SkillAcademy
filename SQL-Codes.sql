de23c70310b46220086f6febcfaa9685905f8b04
Arthur Manske <130318764+arthur-manske@users.noreply.github.com>
2024-09-19 20:12:56 +0000

    Update SQL-Codes.sql

--- SQL-Codes.sql
+++ SQL-Codes.sql
@@ -1,68 +1,69 @@
 -- Tabela de Usuários
-CREATE TABLE Usuarios (
+CREATE TABLE users (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   nome VARCHAR(510) NOT NULL,
+   name VARCHAR(510) NOT NULL,
    email VARCHAR(254) NOT NULL UNIQUE,
-   senha VARCHAR(255) NOT NULL,
-   criado_em DATETIME NOT NULL,
-   perfil_descricao TEXT,
-   genero VARCHAR(62),
+   password VARCHAR(256) NOT NULL,
+   date DATETIME NOT NULL,
+   description TEXT,
+   gender VARCHAR(62),
    premium DATETIME,
-   tipo INT NOT NULL
+   userType INT NOT NULL
 );
  
 -- Tabela de Aulas
-CREATE TABLE Aulas (
+CREATE TABLE classes (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   instrutor_id INT NOT NULL,
-   titulo VARCHAR(255) NOT NULL,
-   descricao TEXT,
-   data DATETIME NOT NULL,
-   preco DECIMAL(10, 2) NOT NULL,
-   arquivos_extras JSON,
-   FOREIGN KEY (instrutor_id) REFERENCES Usuarios(id)
+   instructorId INT NOT NULL,
+   title VARCHAR(255) NOT NULL,
+   description TEXT,
+   creationDate DATETIME NOT NULL,
+   cost DECIMAL(10, 2) NOT NULL,
+   extraFiles TEXT[],
+   tags TEXT[],
+   FOREIGN KEY (instructorId) REFERENCES users(id)
 );
  
 -- Tabela de Compras
-CREATE TABLE Compras (
+CREATE TABLE sells (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   aluno_id INT NOT NULL,
-   aula_id INT NOT NULL,
-   data_compra DATETIME NOT NULL,
-   preco DECIMAL(10, 2) NOT NULL,
-   FOREIGN KEY (aluno_id) REFERENCES Usuarios(id),
-   FOREIGN KEY (aula_id) REFERENCES Aulas(id)
+   studentId INT NOT NULL,
+   classId INT NOT NULL,
+   date DATETIME NOT NULL,
+   cost DECIMAL(10, 2) NOT NULL,
+   FOREIGN KEY (studentId) REFERENCES users(id),
+   FOREIGN KEY (classId) REFERENCES classes(id)
 );
  
 -- Tabela de Avaliações
-CREATE TABLE Avaliacoes (
+CREATE TABLE avaliations (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   aula_id INT NOT NULL,
-   aluno_id INT NOT NULL,
-   nota DECIMAL(1, 1) NOT NULL,
-   comentario TEXT,
-   data_avaliacao DATETIME NOT NULL,
-   FOREIGN KEY (aula_id) REFERENCES Aulas(id),
-   FOREIGN KEY (aluno_id) REFERENCES Usuarios(id)
+   classId INT NOT NULL,
+   studentId INT NOT NULL,
+   note DECIMAL(1, 1) NOT NULL,
+   contents TEXT,
+   date DATETIME NOT NULL,
+   FOREIGN KEY (classId) REFERENCES classes(id),
+   FOREIGN KEY (studentId) REFERENCES users(id)
 );
  
 -- Tabela de Mensagens
-CREATE TABLE Mensagens (
+CREATE TABLE messages (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   remetente_id INT NOT NULL,
-   destinatario_id INT NOT NULL,
-   conteudo TEXT NOT NULL,
-   enviado_em DATETIME NOT NULL,
-   FOREIGN KEY (remetente_id) REFERENCES Usuarios(id),
-   FOREIGN KEY (destinatario_id) REFERENCES Usuarios(id)
+   senderId INT NOT NULL,
+   recipientId INT NOT NULL,
+   contents TEXT NOT NULL,
+   date DATETIME NOT NULL,
+   FOREIGN KEY (senderId) REFERENCES users(id),
+   FOREIGN KEY (recipientId) REFERENCES users(id)
 );
  
 -- Tabela de Fórum
-CREATE TABLE Forum (
+CREATE TABLE forum (
    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-   autor_id INT NOT NULL,
-   titulo VARCHAR(255) NOT NULL,
-   conteudo TEXT NOT NULL,
-   data_postagem DATETIME NOT NULL,
-   FOREIGN KEY (autor_id) REFERENCES Usuarios(id)
+   authorId INT NOT NULL,
+   title VARCHAR(255) NOT NULL,
+   contents TEXT NOT NULL,
+   date DATETIME NOT NULL,
+   FOREIGN KEY (authorId) REFERENCES users(id)
 );
